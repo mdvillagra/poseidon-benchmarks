78aadadd53f830d8b4796beaacfca489e5dc0ae7
mdvillagra <mdvillagra@gmail.com>
2023-08-24 21:01:52 -0400

    added poseidon_ark_hash function

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,21 @@ use crate::poseidon_ark::lib::Poseidon;
 
 
 fn main() {
-    let b1: Fr = Fr::from_str(
-        "0",
-    )
-    .unwrap();
+    let cad = "402384238";
 
-    let mut big_arr: Vec<Fr> = Vec::new();
-    big_arr.push(b1.clone());
-
-    let poseidon = Poseidon::new();
-    let h = poseidon.hash(big_arr.clone()).unwrap();
-
-    println!("The hash is: {}", h.to_string());
+    let new_h = poseidon_ark_hash(cad);
+    println!("The new hash is: {}", new_h);
 }
 
+//Calls the poseidon hash based on arkworks
+//Input is a number in the scalar field F_r of BN254
+//https://docs.rs/ark-bn254/latest/ark_bn254/
+//r = 21888242871839275222246405745257275088548364400416034343698204186575808495617
+fn poseidon_ark_hash(s: &str) -> String {
+    let b: Fr = Fr::from_str(s).unwrap();
+    let mut big_arr: Vec<Fr> = Vec::new();
+    big_arr.push(b.clone());
+    let poseidon = Poseidon::new();
+    let h = poseidon.hash(big_arr.clone()).unwrap();
+    h.to_string()
+}
