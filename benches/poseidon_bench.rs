808bb946a64d79f143cdb8eb4b4d3cb3cfa6d2ac
mdvillagra <mdvillagra@gmail.com>
2023-09-23 21:53:21 -0400

    added risc0 hash to benchmarks

--- benches/poseidon_bench.rs
+++ benches/poseidon_bench.rs
@@ -25,6 +25,13 @@ use neptune::sponge::vanilla::*;
 use rand_xorshift::XorShiftRng;
 use typenum::U4;
 
+//risc0
+use risc0_core::field::{
+    baby_bear::{BabyBear, BabyBearElem, BabyBearExtElem},
+    Elem, ExtElem,
+};
+use risc0_zkp::core::hash::poseidon_254::{self, Poseidon254HashSuite};
+
 fn poseidon_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Poseidon");
     group.sampling_mode(SamplingMode::Flat);
@@ -36,6 +43,7 @@ fn poseidon_benchmark(c: &mut Criterion) {
     let mut dusk_input: Vec<dusk_BlsScalar> = Vec::new();
     let mut lambda_input: Vec<BLS12381FieldElement> = Vec::new();
     let mut neptune_input: Vec<FrNeptune> = Vec::new();
+    let mut risc0_input: Vec<BabyBearElem> = Vec::new();
 
     //rngs
     let dusk_rng = &mut StdRng::seed_from_u64(0xc10d);
@@ -63,6 +71,10 @@ fn poseidon_benchmark(c: &mut Criterion) {
             );
             let element = BLS12381FieldElement::from_hex(&hex_input[0..]).unwrap();
             lambda_input.push(element);
+            //risc0 input
+            risc0_input.push(BabyBearElem::from(XorShiftRng::gen::<u32>(
+                &mut neptune_rng,
+            )));
         }
 
         //Poseidon instantiations
@@ -70,6 +82,16 @@ fn poseidon_benchmark(c: &mut Criterion) {
         let neptune_constants = Sponge::<FrNeptune, U4>::simplex_constants(n_elems);
         let mut neptune_sponge = Sponge::new_with_constants(&neptune_constants, Mode::Duplex);
         let acc = &mut (); //necesary for neptune
+        let risc0_pos = Poseidon254HashSuite::new_suite();
+
+        risc0_pos.hashfn.hash_elem_slice(&risc0_input);
+
+        //risc0 test
+        group.bench_with_input(
+            BenchmarkId::new("Risc0", rounds as u32),
+            &risc0_input,
+            |b, risc0_input| b.iter(|| risc0_pos.hashfn.hash_elem_slice(&risc0_input)),
+        );
 
         //neptune test
         group.bench_with_input(
