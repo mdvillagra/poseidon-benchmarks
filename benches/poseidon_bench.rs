d4803aff97ceef8983301b8c074c960546bfb537
mdvillagra <mdvillagra@gmail.com>
2023-10-03 20:46:40 -0300

    deleted lambdaworks from the benchmarks

--- benches/poseidon_bench.rs
+++ benches/poseidon_bench.rs
@@ -6,24 +6,19 @@ use std::time::Duration;
 
 //sponge for cryptoexperts permutation
 mod sponge;
-use sponge::*;
 use rand::Rng;
+use sponge::*;
 
 //dusk-network
 use dusk_bls12_381::BlsScalar as dusk_BlsScalar;
 use dusk_poseidon::sponge::hash as dusk_hash;
-
-//lambdaworks
-use hex_wrapper::Hex64;
-use lambdaworks_crypto::hash::poseidon as lambda_poseidon;
-use lambdaworks_math::elliptic_curve::short_weierstrass::curves::bls12_381::curve::BLS12381FieldElement;
 
 //neptune
 use blstrs::Scalar as FrNeptune;
 use ff::Field;
 use neptune::sponge::vanilla::*;
 use rand_xorshift::XorShiftRng;
-use typenum::U4;
+use typenum::U3;
 
 //risc0
 use rand::prelude::*;
@@ -37,12 +32,11 @@ fn poseidon_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Poseidon");
     group.sampling_mode(SamplingMode::Flat);
 
-    let n_inputs: u32 = 1; //number of inputs to try
+    let n_inputs: u32 = 0; //number of inputs to try
     let n_elems: usize = 4; //number of elements per try
 
     //input vectors initialization
     let mut dusk_input: Vec<dusk_BlsScalar> = Vec::new();
-    let mut lambda_input: Vec<BLS12381FieldElement> = Vec::new();
     let mut neptune_input: Vec<FrNeptune> = Vec::new();
     let mut risc0_input: Vec<BabyBearElem> = Vec::new();
     let mut cryptoexperts_input: Vec<felt_t> = Vec::new();
@@ -62,39 +56,31 @@ fn poseidon_benchmark(c: &mut Criterion) {
             dusk_input.push(dusk_BlsScalar::random(dusk_rng));
             //neptune input preparation
             neptune_input.push(FrNeptune::random(&mut neptune_rng));
-            //lambdaworks input preparation
-            let hex_input = format!(
-                "{}{}{}{}{}{}",
-                Hex64::rand().to_string(),
-                Hex64::rand().to_string(),
-                Hex64::rand().to_string(),
-                Hex64::rand().to_string(),
-                Hex64::rand().to_string(),
-                Hex64::rand().to_string()
-            );
-            let element = BLS12381FieldElement::from_hex(&hex_input[0..]).unwrap();
-            lambda_input.push(element);
             //risc0 input
             risc0_input.push(BabyBearElem::from(XorShiftRng::gen::<u32>(
                 &mut neptune_rng,
             )));
             //cryptoexperts input
-            cryptoexperts_input.push([ce_rng.gen::<u64>(), ce_rng.gen::<u64>(), ce_rng.gen::<u64>(), ce_rng.gen::<u64>()]);
+            cryptoexperts_input.push([
+                ce_rng.gen::<u64>(),
+                ce_rng.gen::<u64>(),
+                ce_rng.gen::<u64>(),
+                ce_rng.gen::<u64>(),
+            ]);
         }
 
         //Poseidon instantiations
-        let lambda_pos = lambda_poseidon::Poseidon::new();
         let neptune_constants = Sponge::<FrNeptune, U3>::simplex_constants(n_elems);
         let mut neptune_sponge = Sponge::new_with_constants(&neptune_constants, Mode::Simplex);
         let acc = &mut (); //necesary for neptune
         let risc0_pos = Poseidon254HashSuite::new_suite();
-        let mut ce_input_copy = cryptoexperts_input.clone(); 
+        let mut ce_input_copy = cryptoexperts_input.clone();
 
         //cryptoexperts test
         group.bench_with_input(
             BenchmarkId::new("Cryptoexperts", rounds as u32),
             &cryptoexperts_input,
-            |b, cryptoexperts_input| b.iter(|| black_box(hash(&mut ce_input_copy,3))),
+            |b, cryptoexperts_input| b.iter(|| black_box(hash(&mut ce_input_copy, 3))),
         );
 
         //dusk-network test
@@ -123,13 +109,6 @@ fn poseidon_benchmark(c: &mut Criterion) {
                     })
                 })
             },
-        );
-
-        //lambdaworks test
-        group.bench_with_input(
-            BenchmarkId::new("Lambdaworks", rounds as u32),
-            &lambda_input[0..],
-            |b, lambda_input| b.iter(|| black_box(lambda_pos.hash(&lambda_input[0..]))),
         );
 
         //group.significance_level(0.05).sample_size(100).measurement_time(Duration::from_secs(11));
